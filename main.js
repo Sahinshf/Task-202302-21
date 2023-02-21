ddb0efefc0dff56ce486ef17125e801fa7a289d9
Sahinshf <shahinrsha@code.edu.az>
2023-02-21 16:57:19 +0400

    Task ic completed

--- main.js
+++ main.js
@@ -1,44 +1,47 @@
 let button = document.querySelector(".todo_list_button");
 let todo_list = document.querySelector(".todo_list_item");
 let closee = document.getElementsByClassName("closee");
-
-// let i;
-// for (i = 0; i < close.length; i++) {
-//   close[i].onclick = function () {
-//     let div = this.parentElement;
-//     div.style.display = "none";
-//   };
-// }
+let close_btn;
 
 let addList = function () {
   let li = document.createElement("li");
   let input_text = document.querySelector(".todo_list_input").value;
-  document.querySelector(".todo_list_input").value = "";
   let li_text = document.createTextNode(input_text);
-  li.appendChild(li_text);
+  document.querySelector(".todo_list_input").value = "";
 
-  let close_btn = document.createElement("SPAN");
-  let close_txt = document.createTextNode("Delete");
+  if (input_text === "") {
+    alert("You must write something!");
+  } else {
+    li.appendChild(li_text);
+    close_btn = document.createElement("SPAN");
+    let close_txt = document.createTextNode("Delete");
 
-  li.className = "list";
-  close_btn.className = "closee";
-  close_btn.appendChild(close_txt);
-  li.appendChild(close_btn);
-  todo_list.appendChild(li);
+    li.className = "list";
+    close_btn.classList.add("closee");
+    close_btn.appendChild(close_txt);
+    li.appendChild(close_btn);
+    todo_list.appendChild(li);
+  }
 
-  //   for (i = 0; i < close.length; i++) {
-  //     close[i].onclick = function () {
-  //       let div = this.parentElement;
-  //       div.style.display = "none";
-  //     };
-  //   }
+  close_btn.addEventListener("click", function () {
+    todo_list.removeChild(li);
+  });
 };
-console.log(closee);
 
 button.addEventListener("click", addList);
 
-let index;
+// console.log(close_btn);
+// console.log(todo_list);
+// console.log(li);
+// let index;
+// console.log(closee);
 
+//   for (i = 0; i < close.length; i++) {
+//     close[i].onclick = function () {
+//       let div = this.parentElement;
+//       div.style.display = "none";
+//     };
+//   }
 // for (let i = 0, len = closee.children.length; i < len; i++) {
 //   (function (index) {
 //     closee.children[i].onclick = function () {
